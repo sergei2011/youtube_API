skypro-008/youtube-analytics-project

Change sample channels

homework-2/main.py
@@ -1,20 +1,20 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
 
     # получаем значения атрибутов
-    print(vdud.title)  # вДудь
-    print(vdud.video_count)  # 163 (может уже больше)
-    print(vdud.url)  # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
+    print(moscowpython.title)  # MoscowPython
+    print(moscowpython.video_count)  # 685 (может уже больше)
+    print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
     # менять не можем
-    vdud.channel_id = 'Новое название'
+    moscowpython.channel_id = 'Новое название'
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
     # можем получить объект для работы с API вне класса
     print(Channel.get_service())
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
 
-    # создаем файл 'vdud.json' в данными по каналу
-    vdud.to_json('vdud.json')
+    # создаем файл 'moscowpython.json' в данными по каналу
+    moscowpython.to_json('moscowpython.json')

sergei2011/youtube_API

homework-2/main.py
@@ -1,20 +1,21 @@
 from src.channel import Channel
 
 if __name__ == '__main__':
-    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
 
     # получаем значения атрибутов
-    print(vdud.title)  # вДудь
-    print(vdud.video_count)  # 163 (может уже больше)
-    print(vdud.url)  # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
+    print(moscowpython.title)  # MoscowPython
+    print(moscowpython.video_count)  # 685 (может уже больше)
+    print(moscowpython.url)  # https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A
 
     # менять не можем
-    vdud.channel_id = 'Новое название'
-    # AttributeError: property 'channel_id' of 'Channel' object has no setter
+    moscowpython.__channel_id = 'Новое название'
+    # AttributeError: property '__channel_id' of 'Channel' object has no setter
 
     # можем получить объект для работы с API вне класса
     print(Channel.get_service())
     # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
 
-    # создаем файл 'vdud.json' в данными по каналу
-    vdud.to_json('vdud.json')
+    # создаем файл 'moscowpython.json' в данными по каналу
+    moscowpython.to_json('moscowpython.json')
+